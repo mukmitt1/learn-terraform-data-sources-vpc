b10b5768d33b7efcb44382fd35d68f1e430e1914
mukmitt1 <76959601+mukmitt1@users.noreply.github.com>
2022-12-26 12:24:04 -0500

    update

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "vpc" {
 
   cidr = var.vpc_cidr_block
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e"]
+  azs             = data.aws_availability_zones.available.names
   private_subnets = slice(var.private_subnet_cidr_blocks, 0, 2)
   public_subnets  = slice(var.public_subnet_cidr_blocks, 0, 2)
 
@@ -39,3 +39,14 @@ module "lb_security_group" {
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
 }
+
+data "aws_availability_zones" "available" {
+  state = "available"
+
+  filter {
+    name   = "zone-type"
+    values = ["availability-zone"]
+  }
+}
+
+data "aws_region" "current" { }
